fc4f66ebcedb60ce22de1b6905985ebdf7e7373e
Abhishek Arora <abhishek.arora@impetus.com>
2023-03-16 12:34:29 +0530

    inserting data for already created table

--- dbscripts/V1.1.2__test.sql
+++ dbscripts/V1.1.2__test.sql
@@ -1,6 +1,8 @@
-CREATE SCHEMA test;
-create table test.first_table
+CREATE SCHEMA IF NOT EXISTS test;
+create table IF NOT EXISTS test.first_table
 (
 	FIRST_NAME VARCHAR,
 	LAST_NAME VARCHAR
 );
+
+INSERT INTO test.first_table values ('Abhishek','Arora');
